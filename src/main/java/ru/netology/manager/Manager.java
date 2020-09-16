0c1d769f166fc798d7af60a99f8532cc6b97b279
Elena Stashevskaya <lenokstash@gmail.com>
2020-09-16 14:10:19 +0300

    task 2

---
+++ src/main/java/ru/netology/manager/Manager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class Manager {
+    private Post[] post;
+
+    public void wallDelete(int ownerId, long id) {
+
+    }
+
+    public Post wallSearch(int ownerID, String domain, String query, int ownersOnly, int count, int offset){
+        return null;
+    }
+}
