992105d33e28bc50f7c9dff935e806be25075c41
Elena Stashevskaya <lenokstash@gmail.com>
2020-09-16 13:59:32 +0300

    v 2

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -4,4 +4,25 @@ public class Post {
     private Like like;
     private Repost repost;
     private Comment comment;
+    private int Id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private int view;
+    private String postType;
+    private String geo;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
+
 }
